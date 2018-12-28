035a927d9393c4b36e0e036e84be454393c2f804
yixvan6 <yixvan6@163.com>
2018-12-28 03:20:36 +0000

    add to cart

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\AddCartRequest;
+use App\Models\CartItem;
+
+class CartController extends Controller
+{
+    public function add(AddCartRequest $request)
+    {
+        $user = $request->user();
+        $skuId = $request->sku_id;
+        $amount = $request->amount;
+
+        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
+            $cart->amount += $amount;
+        } else {
+            $cart = new CartItem(['amount' => $amount]);
+            $cart->user()->associate($user);
+            $cart->productSku()->associate($skuId);
+        }
+        $cart->save();
+
+        return [];
+    }
+}
