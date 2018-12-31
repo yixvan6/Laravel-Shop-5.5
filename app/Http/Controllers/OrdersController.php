e38bdacfc4b55f84b394d9b54ed4fd81d67d946e
yixvan6 <yixvan6@163.com>
2018-12-31 07:16:19 +0000

    用户界面 - order show page

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -79,4 +79,11 @@ class OrdersController extends Controller
 
         return view('orders.index', compact('orders'));
     }
+
+    public function show(Request $request, Order $order)
+    {
+        $this->authorize('own', $order);
+
+        return view('orders.show', ['order' => $order->load(['items.product', 'items.productSku'])]);
+    }
 }
