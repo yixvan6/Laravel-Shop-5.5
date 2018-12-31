4a87d53e096c8745e616cc86e188fe56d38876b9
yixvan6 <yixvan6@163.com>
2018-12-31 06:37:47 +0000

    close order job

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,7 @@ use App\Models\UserAddress;
 use App\Models\ProductSku;
 use App\Models\Order;
 use Carbon\Carbon;
+use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {
@@ -58,6 +59,8 @@ class OrdersController extends Controller
 
             $skuIds = collect($items)->pluck('sku_id');
             $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+
+            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
             return $order;
         });
---
+++ app/Jobs/CloseOrder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Models\Order;
+
+class CloseOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order, $delay)
+    {
+        $this->order = $order;
+        $this->delay($delay);
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // 判断是否订单已支付
+        if ($this->order->paid_at) {
+            return;
+        }
+
+        // 若没有支付则关闭订单
+        \DB::transaction(function () {
+            $this->order->update(['closed' => true]);
+            // 回退库存
+            foreach ($this->order->items as $item) {
+                $item->productSku->addStock($item->amount);
+            }
+        });
+    }
+}
