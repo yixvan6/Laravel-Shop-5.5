ffae02813dc476194270f402adeee75ffe441563
yixvan6 <yixvan6@163.com>
2019-01-02 09:40:51 +0000

    确认收货

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\UserAddress;
 use App\Models\Order;
 use App\Services\OrderService;
+use App\Exceptions\InvalidRequestException;
 
 class OrdersController extends Controller
 {
@@ -34,5 +35,19 @@ class OrdersController extends Controller
         $this->authorize('own', $order);
 
         return view('orders.show', ['order' => $order->load(['items.product', 'items.productSku'])]);
+    }
+
+    public function received(Request $request, Order $order)
+    {
+        $this->authorize('own', $order);
+
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('物流状态错误');
+        }
+
+        // 更新物流状态
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        return $order;
     }
 }
